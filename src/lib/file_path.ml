070753b777174dd6318da0d707fb8183a700994f
hbr <helmut.brandl@gmx.net>
2022-03-18 17:02:33 +0100

    Added 'File_path' split, join and relative plus docu
    	modified:   src/lib/file_path.ml
    	modified:   src/lib/file_path.mli

--- src/lib/file_path.ml
+++ src/lib/file_path.ml
@@ -90,18 +90,125 @@ let resolve (sep: char) (paths: string list): string =
 
 
 
-let join (sep: char) (paths: string list): string =
-    let concat = String.concat (String.make 1 sep)
+
+let split (sep: char) (path: string): string list =
+    match String.split_on_char sep path with
+    | [] ->
+        assert false (* cannot happen *)
+    | [""] ->
+        ["."]
+    | [""; ""] ->
+        [String.make 1 sep]
+    | "" :: tl ->
+        String.make 1 sep :: tl
+    | lst ->
+        lst
+
+
+
+
+(* Test split *)
+let%test _ =
+    let tests: (string * string list) list =
+        [
+            "/a/b/c",               ["/"; "a"; "b"; "c"];
+            "/",                    ["/"];
+        ]
     in
-    match
-        paths
-    with
-    | hd :: tl when String.length hd = 1 && hd.[0] = sep ->
-        (* When head is only the root ["/"], then no separator shall be
-           inserted *)
-        hd ^ concat tl
-    | paths ->
-        concat paths
+    List.for_all
+        (fun (path, expect_lst) ->
+             let lst = split '/' path in
+             if lst <> expect_lst then begin
+                 let string_of lst =
+                     "[" ^ String.concat ", " lst ^ "]"
+                 in
+                 Printf.printf "result %s, expect %s\n"
+                     (string_of lst) (string_of expect_lst)
+             end;
+             lst = expect_lst)
+        tests
+
+
+
+
+
+let join (sep: char) (paths: string list): string =
+    (* The paths have to be normalized and only the first one can be absolute.
+
+       If this is not the case, better use [resolve].
+     *)
+    let sep = String.make 1 sep in
+    let rec join = function
+        | [] ->
+            "."
+        | [name] ->
+            if name = "" then
+                "."
+            else
+                name
+        | hd :: tl ->
+            let str = join tl in
+            if hd = "." && str = "." then
+                "."
+            else if hd = sep && str = "." then
+                hd
+            else if hd = "." || hd = "" then
+                str
+            else if hd = sep then
+                hd ^ str
+            else if str = "." then
+                hd
+            else
+                hd ^ sep ^ str
+    in
+    join paths
+
+
+
+let relative (sep: char) (from: string) (to_: string): string =
+    (* Both must be absolute and normalized and therefore not empty. *)
+    let rec up res = function
+        | [] ->
+            res
+        | hd :: tl when hd = String.make 1 sep ->
+            up res tl
+        | _ :: tl ->
+            up (".." :: res) tl
+    in
+    let rec rel lst1 lst2 =
+        match lst1, lst2 with
+        | hd1 :: tl1, hd2 :: tl2 when hd1 = hd2 ->
+            (* Still in common prefix. *)
+            rel tl1 tl2
+        | lst1, lst2 ->
+            (* Common prefix has been skipped. *)
+            up [] lst1 @ lst2 |> join sep
+    in
+    rel (split sep from) (split sep to_)
+
+
+
+
+(* Test relative *)
+let%test _ =
+    let tests: (string * string * string) list =
+        [
+            "/",        "/b",       "b";
+            "/a",       "/b",       "../b";
+            "/a/b",     "/b",       "../../b";
+        ]
+    in
+    List.for_all
+        (fun (a, b, expect) ->
+             let res = relative '/' a b in
+             if res <> expect then
+                 Printf.printf
+                     "test relative: a <%s>, b <%s>, res <%s>, expect <%s>\n"
+                     a b res expect;
+             res = expect)
+        tests
+
+
 
 
 
@@ -136,6 +243,11 @@ let basename (sep: char) (path: string): string =
     | Some i ->
         assert (i + 1 <= len);
         String.sub path (i + 1) (len - (i + 1))
+
+
+
+
+
 
 
 
@@ -187,6 +299,7 @@ let%test _ =
         tests
 
 
+
 (* Test resolve *)
 let%test _ =
     let tests: (string list * string) list =
@@ -216,7 +329,38 @@ let%test _ =
         tests
 
 
-(* Test dirname *)
+
+
+(* Test join *)
+let%test _ =
+    let tests: (string list * string) list =
+        [
+            ["."; "a"],         "a";
+
+            ["."; "."],         ".";
+
+            ["a"; "b"; "."],    "a/b";
+
+            ["/"; "."; "a"],    "/a";
+
+            ["a/b"; "c/d"],     "a/b/c/d";
+
+            [""; ""],           "."
+        ]
+    in
+    List.for_all
+        (fun (lst, expect) ->
+             let path = join '/' lst in
+             if path <> expect then
+                 Printf.printf
+                     "path <%s>, expect <%s>\n"
+                     path expect;
+             path = expect)
+        tests
+
+
+
+(* Test dirname and basename *)
 let%test _ =
     let tests: (string * string * string) list =
         [
--- src/lib/file_path.mli
+++ src/lib/file_path.mli
@@ -1,17 +1,87 @@
+(** Process File and Directory Paths *)
+
+
+
+
+(** {1 Normalization}
+
+    Paths to the same file or directory are not unique. The following shows a
+    list of path pairs which are equivalent.
+
+    {[
+        path                        equivalent path
+        "././."                     "."
+
+        "/.."                       "/"
+
+        "a/b/.."                    "a"
+
+        "a//b////c"                 "a/b/c"
+
+        ""                          "."
+    ]}
+
+    The process of {e normalization} removes duplicate separators and redundant
+    redirections. The result of normalization is never the empty string.
+
+
+*)
+
+
 val normalize: char -> string -> string
+(** [normalize sep path] Normalize the path [path] i.e. remove duplicate
+ * separators and redundant [.] and [a/..]. *)
+
 
 val resolve: char -> string list -> string
+(** [resolve sep paths]
 
+    Join the paths in [paths] from right to left, normalize the result and stop
+    joining, when an absolute path is the result. The paths in [paths] need not
+    be normalized. The result is normalized.
+*)
+
+
+
+
+(** {1 Split and Join} *)
 
 val join: char -> string list -> string
 (** [join sep paths]
 
-    The string in the list [paths] are joined by inserting a separator between
-    them. If the first element of the paths is the root (i.e. ["/"] on unix
-    systems), then the separator is not duplicated.
+    Join the normalized paths in the list [paths]. All elements must be in
+    normal form and all except the first must be relative.
 
-    If all paths are normalized, then the result is normalized as well.
+    If the precondition is not satisfied, better use {!resolve}.
+
+    If the precondition is satisfied, then the result is guaranteed to be
+    normalized i.e. redundant paths ["."] are removed.
 *)
+
+
+val split: char -> string -> string list
+(** [split sep path]
+
+    Split the normalized path into its components. If the input is normalized,
+    then the components are normalized as well. If the path is absolute, then
+    the first element in the result list is the separator i.e. ["/"] on unix
+    systems.
+
+    Join and split are invers operations.
+*)
+
+
+
+(** {1 Relative Paths} *)
+
+val relative: char -> string -> string -> string
+(** [relative char a b]
+
+    Assume that [a] and [b] are absolute paths. Construct a relative path from
+    [a] to [b].
+*)
+
+(** {1 Dirname and Basename} *)
 
 val dirname: char -> string -> string
 
