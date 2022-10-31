0537717c6d0c5f339ed69887d621e73b02505374
hbr <helmut.brandl@gmx.net>
2022-10-31 06:58:03 -0600

    Introduced values into terms and some docu added
    	modified:   src/core/core_docu.mld
    	modified:   src/core/term.ml

--- src/core/term.ml
+++ src/core/term.ml
@@ -1,3 +1,5 @@
+module type ANY = Fmlib_std.Interfaces.ANY
+
 module Binder_info =
     struct
         type t = {
@@ -9,14 +11,19 @@ module Binder_info =
     end
 
 
-type t =
-    | Zero         (* Lowest predicative universe *)
-    | Succ of t    (* Next predicative universe *)
-    | Prop
-    | Any of t
-    | Local of int (* De Bruijn index *)
-    | Meta  of int (* De Bruijn level *)
-    | Pi of  binder array * t
-    | Lam of binder array * t
 
-and binder = Binder_info.t * t
+module Make (V: ANY) =
+struct
+    type t =
+        | Zero         (* Lowest predicative universe *)
+        | Succ of t    (* Next predicative universe *)
+        | Prop
+        | Any of t
+        | Local of int (* De Bruijn index *)
+        | Meta  of int (* De Bruijn level *)
+        | Pi of  binder array * t
+        | Lam of binder array * t
+        | Value of V.t
+
+    and binder = Binder_info.t * t
+end
